8792e7ef675a41068a6c6f9451c350f808de4504
Pratham Jain <jainpratham2601@gmail.com>
2026-03-22 12:59:37 +0530

    observer pattern push completed

--- 02_Observer_Pattern/Self_Implementation/Push/WorkStation.java
+++ 02_Observer_Pattern/Self_Implementation/Push/WorkStation.java
@@ -9,3 +9,8 @@ public class WorkStation {
         wd.measurementsChanged(22.2f, 48.0f, 70.0f);
     }
 }
+
+// oo principle 
+//     strive for loosely coupled designs between objects that interact.
+// oo patterns
+//     defines a one to many dependency between objects so that when one object changes all its dependants get notified and updated automatically.  
